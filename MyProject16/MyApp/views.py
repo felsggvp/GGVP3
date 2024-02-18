bda5f5b7ac6b03043f0a453ee75227b3c3412de0
felsggvp <ff.pp.22@mail.ru>
2024-02-18 18:36:48 +0300

    ggvp

--- MyProject16/MyApp/views.py
+++ MyProject16/MyApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloworld(request):
-    return render(request, 'helloworld.html')
+    return render(request, 'hello_world.html')
