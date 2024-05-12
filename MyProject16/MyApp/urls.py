24f0c6ef8b7d494fe704217987bac8616747b7d9
felsggvp <ff.pp.22@list.ru>
2024-05-12 18:28:34 +0300

    ggvp

--- MyProject16/MyApp/urls.py
+++ MyProject16/MyApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("kk/", views.helloworld, name="helloworld"),
-    path("oguzok/", views.oguzik, name="oguzik")
+    path('kk/', views.helloworld, name="helloworld"),
+    path('oguzok/', views.oguzik, name="oguzik"),
+    path('', views.oguzik, name="chuhu")
 ]
